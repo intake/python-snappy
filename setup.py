21c7a338c40dae5cbdc5b0216102465eb7ae14d8
Gábor Lipták <gliptak@gmail.com>
2024-02-02 13:25:46 -0500

    Add Python 3.11 to GHA

--- setup.py
+++ setup.py
@@ -91,7 +91,8 @@ setup(
                  'Programming Language :: Python :: 3.7',
                  'Programming Language :: Python :: 3.8',
                  'Programming Language :: Python :: 3.9',
-                 'Programming Language :: Python :: 3.10'
+                 'Programming Language :: Python :: 3.10',
+                 'Programming Language :: Python :: 3.11'
                  ],
     ext_modules=ext_modules,
     packages=packages,
